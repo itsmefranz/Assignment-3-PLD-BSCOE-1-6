f707bc2ec516935dd52f0340be4b6d0fabc74112
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-30 22:35:31 +0800

    code complete

--- program1_v2.py
+++ program1_v2.py
@@ -3,7 +3,16 @@ def getName():
     _name= input("Name: ")
     return _name 
 
+def getAge():
+    _age= input("Age: ")
+    return _age
 
+def getAddress():
+    _add= input("Address: ")
+    return _add
+
+def display(nameF, ageF, addF):
+    print(f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addF}.")
 
 #steps
 #1. ask for name then save as variable
@@ -11,7 +20,7 @@ name= getName()
 #2. ask for age then save as variable
 age= getAge()
 #3. ask for address then save as variable
-
+address= getAddress()
 #4. display the details
 
 display(name, age, address)
