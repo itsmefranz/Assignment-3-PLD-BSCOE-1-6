460092f99f20ec230bcceb11ec87d4e56aa8da02
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-30 22:43:07 +0800

    first upload

---
+++ program2_v2.py
@@ -0,0 +1,17 @@
+#Create a program that will ask how many apples and oranges you want to buy.
+#Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+#Display the output in the following format.
+#The total amount is ______.
+
+def getApple():
+    _apple= input("Please indicate how many apples you want to purchase: ")
+    return _apple
+
+
+
+#steps
+#1. state price of apple.
+appleprice= getApple()
+#2. state price of orange.
+
+#3. display total amount.
