556069de4fac289dc1c1f6ba528af31bb50ec5eb
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-10-31 22:48:30 +0800

    code complete

--- program3_v2.py
+++ program3_v2.py
@@ -1,12 +1,26 @@
 
 def getMoney():
-    amountM= float(input("How much money do you have? "))
+    amountM= int(input("How much money do you have? "))
     return amountM
 
+def getApple():
+    appleprice= float(input("What is the price of an apple per piece? "))
+    return appleprice
+
+def getChange():
+    change = float(money % appleP)
+    return change
 
 
+def getAmount(money, appleP):
+    maxnoApl= int(money // appleP)
+    return maxnoApl
 
 
 money= getMoney()
 
 appleP= getApple()
+_change= getChange()
+totalamount= getAmount(money, appleP)
+
+print(f"You can buy {totalamount} apples and your change is PHP{_change: .2f}")
